9b070205449413abd810098c5579dfc00d3d6280
loveangelsa <155241488+loveangelsa@users.noreply.github.com>
2024-02-17 02:51:16 +0900

    Update ezville_wallpad.py

--- ezville_wallpad/ezville_wallpad.py
+++ ezville_wallpad/ezville_wallpad.py
@@ -35,8 +35,8 @@ RS485_DEVICE = {
         "query":    { "id": 0x36, "cmd": 0x01, },
         "state":    { "id": 0x36, "cmd": 0x81, },
         "last":     { },
-
-        "away":    { "id": 0x36, "cmd": 0x45, "ack": 0x00, },
+        "power":    { "id": 0x36, "cmd": 0x43, "ack": 0x03, },
+        "away":    { "id": 0x36, "cmd": 0x45, "ack": 0x05, },
         "target":   { "id": 0x36, "cmd": 0x44, "ack": 0xC4, },
     },
     "batch": {  # 안보임
